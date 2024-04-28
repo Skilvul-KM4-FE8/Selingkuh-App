8aec5c9dd0231da6b7bd1173ac8e59d1a4ac0a8f
alfitosantosa <44111010166@student.mercubuana.ac.id>
2024-04-28 22:35:53 +0700

    create form prisma.orm

--- src/app/api/register.js
+++ src/app/api/register.js
@@ -1,29 +1,29 @@
-const { PrismaClient } =require("@prisma/client")
+const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-async function main() {
-    // prisma queries will be here
+async function main(req, res) {
+  // prisma queries will be here
 
-    // addData
-    await prisma.user.create({
-        data: {
-            name: "Rafai (dev)",
-            email: "devtest@gmail.com",
-            password: "test123"
-        }
-    })
+  // addData
+  await prisma.user.create({
+    data: {
+      name: req.user.name,
+      email: req.user.email,
+      password: req.user.password,
+    },
+  });
 
-    const allUsers = await prisma.user.findMany()
-    console.log(allUsers)
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
 }
 
 main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.log(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.log(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
