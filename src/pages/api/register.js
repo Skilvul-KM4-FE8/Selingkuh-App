aa0e410711500a8fb172c50414f602308b790452
alfitosantosa <44111010166@student.mercubuana.ac.id>
2024-04-28 23:29:14 +0700

    new branch

--- src/pages/api/register.js
+++ src/pages/api/register.js
@@ -2,35 +2,26 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-
 export default async function handler(req, res) {
-    const result = req.body
-    console.log(result)
-    
+  const result = req.body;
+  console.log(result);
 
-<<<<<<< HEAD
-  res.status(200).json({ message: "User created successfully" });
+  // prisma queries will be here
+
+  // addData
+  await prisma.user.create({
+    data: {
+      name: result.name,
+      email: result.email,
+      password: result.password,
+    },
+  });
 
   const allUsers = await prisma.user.findMany();
   console.log(allUsers);
-=======
-    // prisma queries will be here
 
-    // addData
-    await prisma.user.create({
-        data: {
-            name: result.name,
-            email: result.email,
-            password: result.password
-        }
-    })
-
-    const allUsers = await prisma.user.findMany()
-    console.log(allUsers)
-
-    console.log("Running")
-    return res.status(200).json({status: "success"})
->>>>>>> 681b7b84f116087191708bf775016433eec0dd88
+  console.log("Running");
+  return res.status(200).json({ status: "success" });
 }
 
 // handler()
