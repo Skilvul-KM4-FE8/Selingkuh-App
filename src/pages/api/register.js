6ac2450f70cffc8dd017fe64774e9d99fc370764
Muhammad Rafai <rafaimhd123@gmail.com>
2024-05-05 23:54:01 +0700

    fix register logic

--- src/pages/api/register.js
+++ src/pages/api/register.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
                     password: hash
                 }
             })
-            return res.status(200).json({status: "success"})
+            return res.status(200).json({status:  "success"})
         } catch (error) {
             console.log("===========")
             // console.log(error)
